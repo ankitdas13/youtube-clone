332622cb53eb530c997be8a656470db57e38f864
ankitdas13 <ankit.das@razorpay.com>
2023-11-01 04:26:02 +0530

    env variable fix

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,11 +1,15 @@
-export const YOUTUBE_API = import.meta.env.VITE_YOUTUBE_API
+const API_KEY = import.meta.env.VITE_API_KEY
+
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&regionCode=in&key="+API_KEY+"&maxResults="
 
 export const YOUTUBE_SEARCH = "http://localhost:4001/search?query="
 
-export const YOUTUBE_VIDEO_SEARCH = import.meta.env.VITE_YOUTUBE_VIDEO_SEARCH
+export const YOUTUBE_VIDEO_SEARCH = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=15&key="+API_KEY+"&q="
+
+export const YOUTUBE_PARENT_COMMENTS = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key="+API_KEY+"&videoId="
+
+export const YOUTUBE_VIDEO_DETAILS = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key="+API_KEY+"&id="
+
+export const YOUTUBE_CHANNELS = "https://youtube-search-and-download.p.rapidapi.com/channel/about?id="
 
 export const MAX_RESULT = "15"
-
-export const YOUTUBE_CHANNELS = import.meta.env.VITE_YOUTUBE_CHANNELS
-export const YOUTUBE_PARENT_COMMENTS = import.meta.env.VITE_YOUTUBE_PARENT_COMMENTS
-export const YOUTUBE_VIDEO_DETAILS = import.meta.env.VITE_YOUTUBE_VIDEO_DETAILS
